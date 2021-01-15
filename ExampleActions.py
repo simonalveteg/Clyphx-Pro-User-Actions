231a567a6b33c6c329f8a05d6dabc2f1ba0b529f
a1vv <41792248+a1vv@users.noreply.github.com>
2021-01-16 00:54:38 +0100

    Recplay action finished
    action to record/play a clip in a working (finished?) state

--- ExampleActions.py
+++ ExampleActions.py
@@ -169,7 +169,7 @@ class ExampleActions(UserActionsBase):
 
     # Your class must implement this method.
     def create_actions(self):
-        self.add_track_action('recplay', self.sa_rec)
+        self.add_track_action('rp', self.sa_rec)
         self.add_global_action('drums', self.sa_drums)
 
     def sa_drums(self, action_def, args):
@@ -180,29 +180,39 @@ class ExampleActions(UserActionsBase):
 
     def sa_rec(self, action_def, args):
         """check if track is already recording, play clip (to stop recording) if it is, otherwise start recording"""
-        clip = int(args)
         track = action_def['track']
         tracklist = list(self.song().tracks)
         track_index = tracklist.index(track) + 1
+        cliplist = list(track.clip_slots)
         self.log('Called on track no: %s' % track_index)
-        self.log('Args: %s' % args)
-        for ttrack in self.song().tracks:
-            self.canonical_parent.log_message('looping through tracks, %s' % ttrack)
-            if ttrack == track:
-                self.log('playing_status: %s' % ttrack.clip_slots[clip].playing_status)
-                self.log('is_recording: %s' % ttrack.clip_slots[clip].is_recording)
-                if ttrack.clip_slots[clip].is_recording:
-                    self.log('is recording! start playing')
-                    break
-                else:
-                    self.log('not recording, start recording on next slot')
-                    clip += 1
-                    break
-        action = '%s/play %s' % (track_index, clip + 1)
+        for clip in track.clip_slots:
+            self.log('looping through clips, %s' % clip)
+            self.log('clip index, %s' % cliplist.index(clip))
+            if clip.has_clip:
+                self.log('clip exists')
+            elif cliplist.index(clip) == 0:
+                self.log('index is 0')
+                clipslot = cliplist.index(clip) # no clips -> select first empty clipslot
+                break
+            else:
+                self.log('else')
+                clipslot = cliplist.index(clip) - 1 # clip exists -> select last clip
+                break
+        self.log('playing_status: %s' % track.clip_slots[clipslot].playing_status)
+        self.log('is_recording: %s' % track.clip_slots[clipslot].is_recording)
+        if track.clip_slots[clipslot].is_recording:
+            self.log('is recording! start playing')
+        elif track.clip_slots[clipslot].has_clip:
+            self.log('not recording, start recording on next slot')
+            clipslot += 1
+
+        action = '%s/play %s' % (track_index, clipslot + 1)
         self.canonical_parent.clyphx_pro_component.trigger_action_list(action)
+
+    def sa_pause(self, action_def, args):
+        """ play/stop the latest clip. If clip is recording it should stop the clip """
 
     def toast(self, comments):
         self.canonical_parent.show_message(comments)
-
     def log(self, message):
         self.canonical_parent.log_message(message)
